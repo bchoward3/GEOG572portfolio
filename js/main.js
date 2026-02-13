346a8ab6e98a349eadd3e524b96ca4c402d4df23
bchoward3 <bchoward@wisc.edu>
2026-02-12 21:55:35 -0500

    Updated map
    Updated landing page map to Lonely Plant

--- js/main.js
+++ js/main.js
@@ -7,28 +7,28 @@ function createMap() {
     map = L.map('map').setView([32, -81], 9);
 
     // Add the Alidade Smooth Dark base tilelayer to the map
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+    L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png', {
         minZoom: 0,
         maxZoom: 20,
-        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="http://www.openstreetmap.bzh/" target="_blank">Breton OpenStreetMap Team</a>',
         ext: 'png'
     }).addTo(map);
 
     // Call the function to load GeoJSON data
-    getData();
+    //getData();
 }
 
 // Function to retrieve the GeoJSON data and add it to the map
-function getData() {
-    fetch("data/Chatham_municipalities.geojson")
-        .then(function(response) {
-            return response.json();  // Parse the JSON data
-        })
-        .then(function(json) {
+//function getData() {
+    //fetch("data/Chatham_municipalities.geojson")
+        //.then(function(response) {
+            //return response.json();  // Parse the JSON data
+        //})
+        //.then(function(json) {
             // Create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json).addTo(map);
-        });
-}
+            //L.geoJson(json).addTo(map);
+        //});
+//}
 
 // Call createMap() when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', createMap);
